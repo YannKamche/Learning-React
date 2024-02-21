920bdeb2afdbd8ecf27591cf76c6afe29a31f248
Yann Kamche <yannkamche@gmail.com>
2024-02-21 05:09:54 +0100

    my first react app

---
+++ hello-world/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen flex flex-col justify-center items-center bg-slate-300 gap-3'>
+      <p className='text-3xl font-bold'>Hello World! I am learning React</p>
+      <p className='text-5xl font-semibold text-gray-950 leading-snug tracking-wider'>This is my first React Application</p>
+    </div>
+  )
+}
+
+export default App
