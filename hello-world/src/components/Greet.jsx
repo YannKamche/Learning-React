3dd58f855c4d87fee4582fbfa151358dd262d725
Yann Kamche <yannkamche@gmail.com>
2024-02-22 06:07:04 +0100

    functional components

---
+++ hello-world/src/components/Greet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Greet = () => {
+  return (
+    <h1 className='font-bold text-3xl tracking-widest'>
+      Hi! everybody
+    </h1>
+  )
+}
+
+export default Greet
