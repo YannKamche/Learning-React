230f2947293efe9c3321198b56f73fdcbf1f4786
Yann Kamche <yannkamche@gmail.com>
2024-02-22 07:46:43 +0100

    state managment in class components

--- hello-world/src/components/Welcome.jsx
+++ hello-world/src/components/Welcome.jsx
@@ -1,13 +1,28 @@
 import React , { Component } from 'react'
 
 class Welcome extends Component {
-    
+    constructor() {
+        super()
+        this.state = {
+            message:  `Welcome to the New World`
+        }
+    }
+    //Modify the state
+    updateMessage() {
+        this.setState({
+            message: "Your journey begins Now!"
+        })
+    }
     render() {
         
         return (
             <>
-                <h1 className='font-bold text-3xl tracking-widest'>Hi everybody! My name is {this.props.name} </h1>
-                {this.props.children}
+                <h1 className='font-bold text-3xl tracking-widest'>{this.state.message}</h1> 
+                <button className='p-4 bg-black text-white text-xl font-medium rounded-md shadow-md'
+                onClick={() => this.updateMessage()}
+                >
+                    Click Here
+                </button>   
             </>
         )
     }
