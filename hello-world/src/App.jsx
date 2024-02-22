8d3ab37bab756c5b251c67dd23aaafcd231a35c6
Yann Kamche <yannkamche@gmail.com>
2024-02-22 07:03:41 +0100

    Using Props with functional components

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -6,14 +6,9 @@ import Hello from './components/Hello'
 const App = () => {
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center bg-slate-300 gap-3'>
-      {/* <p className='text-3xl font-bold'>Hello World! I am learning React</p>
-      <p className='text-5xl font-semibold text-gray-950 leading-snug tracking-wider'>This is my first React Application</p> */}
-
-      {/* My first Class components
-      <Greet />
-      <Welcome /> */}
-
-      <Hello />
+      <Greet name="Bruce">
+        <p className='text-2xl font-semibold tracking-tighter'>I am super Hero coming to save you</p>
+      </Greet>
     </div>
   )
 }
--- hello-world/src/components/Greet.jsx
+++ hello-world/src/components/Greet.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Greet = () => {
+const Greet = (props) => {
   return (
-    <h1 className='font-bold text-3xl tracking-widest'>
-      Hi! everybody
+   <>
+     <h1 className='font-bold text-3xl tracking-widest'>
+      Hi everybody! My name is {props.name} 
     </h1>
+    {props.children}
+   </>
+    
   )
 }
 
