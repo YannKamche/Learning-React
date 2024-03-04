2830b6a03f1d5635b413b3280d58d9ffa627ac57
Yann Kamche <yannkamche@gmail.com>
2024-03-04 21:45:50 +0100

    conditionally run effects

--- react-hooks/src/components/Effect.jsx
+++ react-hooks/src/components/Effect.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from 'react'
 
 const Effect = () => {
     const [count, setCount] = useState(0);
-    const handleCount = () => {
-        setCount(count + 1)
-    }
+    const [name, setName] = useState('')
+
+    const handleCount = () => setCount(count + 1)
+
+    const handleChange = e => setName(e.target.value)
 
     useEffect(() => {
+        console.log("Updating the document title")
         document.title = `Clicked ${count}`
-    })
+    }, [count])
   return (
     <div>
       <button 
@@ -16,6 +19,7 @@ const Effect = () => {
       className='bg-black text-white text-3xl p-3 rounded-md'>
         Click me {count}
     </button>
+    <input type="text" name="name" id="" value={name} onChange={handleChange} />
     </div>
   )
 }
