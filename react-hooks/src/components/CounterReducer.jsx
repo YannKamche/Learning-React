e1ec064cda8272380cd8148ff75054be7ffa2ce3
Yann Kamche <yannkamche@gmail.com>
2024-03-09 13:58:18 +0100

    Multiple useReducers

--- react-hooks/src/components/CounterReducer.jsx
+++ react-hooks/src/components/CounterReducer.jsx
@@ -15,15 +15,33 @@ const reducer = (state, action) => {
 }
 const CounterReducer = () => {
 
-    const [count, dispatch] = useReducer(reducer, initialState)
+    const [count, dispatch] = useReducer(reducer, initialState);
+    const [count2, dispatchTwo] = useReducer(reducer, initialState)
   return (
-      <div>
-          {count}
-          <button className="" onClick={() => dispatch('increment')}>Increment</button>
-          <button className="" onClick={() => dispatch('decrement')}>Decrement</button>
-          <button className="" onClick={() => dispatch('reset')}>Reset</button>
+    <div>
+      {count}
+      <button className="" onClick={() => dispatch("increment")}>
+        Increment
+      </button>
+      <button className="" onClick={() => dispatch("decrement")}>
+        Decrement
+      </button>
+      <button className="" onClick={() => dispatch("reset")}>
+        Reset
+      </button>
+
+      {count2}
+      <button className="" onClick={() => dispatchTwo("increment")}>
+        Increment
+      </button>
+      <button className="" onClick={() => dispatchTwo("decrement")}>
+        Decrement
+      </button>
+      <button className="" onClick={() => dispatchTwo("reset")}>
+        Reset
+      </button>
     </div>
-  )
+  );
 }
 
 export default CounterReducer
